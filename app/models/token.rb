a4ee3633dfefc2dcf0dad75f15c38bccd5f2274b
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-04-20 17:03:37 +0200

    Validations

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,6 +1,8 @@
 class Token < ActiveRecord::Base
   belongs_to :badge
 
+  validates :code, :badge_id, presence: true
+
   def available_for user
   	number_of_current_badge = user.awards.where("badge_id = ?", badge.id).count()
   	return number_of_current_badge == 0
