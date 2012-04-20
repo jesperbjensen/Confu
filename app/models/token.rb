b294f13176bf917d0ce06217c0ece12e51c9e19d
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-04-20 17:06:37 +0200

    token work

--- app/models/token.rb
+++ app/models/token.rb
@@ -2,9 +2,10 @@ class Token < ActiveRecord::Base
   belongs_to :badge
 
   validates :code, :badge_id, presence: true
-
+	validates :number_of_times_allowed, :numericality => { :greater_than => 0 }
+  
   def available_for user
   	number_of_current_badge = user.awards.where("badge_id = ?", badge.id).count()
-  	return number_of_current_badge == 0
+  	return number_of_current_badge < number_of_times_allowed
   end
 end
