c1c561a2352d8507fc20a3707bc42ee0cd688e0d
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-04-20 12:35:32 +0200

    made so that a scan is made, but no user is present we will redirect to new user

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -1,4 +1,13 @@
 class ScansController < ApplicationController
   def new
+
+  	if current_user.nil?
+  		redirect_to new_user_path({code: params[:code]})
+  	end
+
+  	@token = Token.find_by_code(params[:code])
+  	@badge = @token.badge
+
+
   end
 end
