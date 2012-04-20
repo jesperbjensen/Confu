39731bd27b891ae32d36f9164eee8c69ab41f858
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-04-20 17:54:08 +0200

    token work

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -11,6 +11,10 @@ class ScansController < ApplicationController
 
   	if @token.available_for(current_user)
   		@award = @badge.award_to(current_user)
+    else
+      if @token.number_of_times_left(current_user) > 0
+        @time_until_unlock = @token.minutes_until_unlock(current_user)
+      end
   	end
   end
 end
--- app/models/token.rb
+++ app/models/token.rb
@@ -5,12 +5,24 @@ class Token < ActiveRecord::Base
 	validates :number_of_times_allowed, :minimum_time_between_scans_in_minutes, :numericality => { :greater_than => 0 }
   
   def available_for user
-  	current_awards = user.awards.where("badge_id = ?", badge.id).order("created_at desc")
-  	last_award = current_awards.first
-  	number_of_current_badge = current_awards.count
+  	return number_of_times_left(user) > 0 && minutes_until_unlock(user) == 0
+  end
 
-  	minutes_since_last = (Time.now - last_award.created_at) / 1.minute
+  def number_of_times_left user
+    awards = user.awards.where("badge_id = ?", badge.id).order("created_at desc")
+    num_of = awards.count
+    return number_of_times_allowed - num_of
+  end
 
-  	return (number_of_current_badge < number_of_times_allowed) && minutes_since_last >= minimum_time_between_scans_in_minutes
+  def minutes_until_unlock user
+    last_award = user.awards.where("badge_id = ?", badge.id).order("created_at desc").first
+    min_since_last = (Time.now - last_award.created_at) / 1.minute
+    time_to_unlock = minimum_time_between_scans_in_minutes - min_since_last
+
+    if time_to_unlock < 0
+      return 0
+    else
+      return time_to_unlock
+    end
   end
 end
