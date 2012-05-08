2cee57383af491ac7f8d0a4f591e102b2abee0e2
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-05-08 20:06:02 +0200

    Shows all badges

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -3,5 +3,7 @@ class AwardsController < ApplicationController
     @user = User.find(params[:user_id])
     @awards = @user.awards
     @badges = @awards.group_by { |t| t.badge_id }
+
+    @badges_not_owned = Badge.where("id not in (?)", @awards.collect {|i| i.badge_id})
   end
 end
