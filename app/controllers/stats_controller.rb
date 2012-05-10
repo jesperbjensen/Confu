7c8fc999d9c1c5aab4996d601c62f81596052586
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-05-10 14:01:46 +0200

    fixes

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,9 +7,9 @@ class StatsController < ApplicationController
     @total_tweets = Tweet.where("user_id is not null").count
     @total_qr_shakes = Shake.all.count / 2
 
-    @toilet = ActiveRecord::Base.connection.execute("SELECT u.name as username, b.name, COUNT(a.badge_id) as quantity from awards a INNER JOIN badges b on b.id = a.badge_id INNER JOIN users u on u.id = a.user_id GROUP BY badge_id, b.name, u.id ORDER BY COUNT(a.badge_id) desc, b.name")
+    @toilet = ActiveRecord::Base.connection.execute("SELECT u.name as username, b.name, COUNT(a.badge_id) as quantity from awards a INNER JOIN badges b on b.id = a.badge_id INNER JOIN users u on u.id = a.user_id GROUP BY u.name, b.name, a.badge_id ORDER BY COUNT(a.badge_id) desc, b.name")
   
-    @top_tweeters = ActiveRecord::Base.connection.execute("SELECT u.name, COUNT(t.id) as quantity from tweets t INNER JOIN users u on u.id = t.user_id GROUP BY u.id ORDER BY COUNT(t.id) desc")
+    @top_tweeters = ActiveRecord::Base.connection.execute("SELECT u.name, COUNT(t.id) as quantity from tweets t INNER JOIN users u on u.id = t.user_id GROUP BY t.id, u.name ORDER BY COUNT(t.id) desc")
   end
 
 end
